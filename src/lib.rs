d5f62d0d6c4094c7c04bae916821a8a7a4d65f18
Valentin Romanov <22451680+valrom@users.noreply.github.com>
2022-02-11 21:27:08 +0300

    It works!!!!

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,47 @@
-use std::thread;
-
-
 // Round 2
 
-const COMP_LIMIT: usize = 10;
+const COMP_LIMIT: usize = 100;
 
-pub fn split_compitation<T ,R, F>(mut data: Vec<T>, f: &F) -> Vec<R>
+pub fn split_compitation<T ,R, F>(data: &Vec<T>, f: &F) -> Vec<R>
     where T : Send + Sync + Copy + std::fmt::Debug + 'static,
     R : Send + Copy + Default + 'static,
     F : Send + Fn(T) -> R + Sync + 'static,
 {
-    let mut out_vec = vec![R::default(); data.len()];
+    let mut out_vec = Vec::with_capacity(data.len());
 
     if data.len() < COMP_LIMIT {
-        for i in 0..data.len() {
-            out_vec[i] = f(data[i]);
+        for i in data {
+            out_vec.push(f(*i));
         }
     } else {
         // Don't sure how much threads to create
         // Let it be 4
         const CHUNKS: usize = 4;
-        let step = CHUNKS / data.len();
+        let step = data.len() / CHUNKS;
         
-        // split data to parts
-        let mut parts = Vec::with_capacity(CHUNKS);
-        for i in data.chunks(step) {
-            parts.push(Vec::from(i));
-        }
-        dbg!(&parts);
+        crossbeam::scope(|s| {
+            let mut joins = Vec::with_capacity(CHUNKS + 1);
+
+            for chunk in data.chunks(step) {
+                joins.push(
+                    s.spawn(move |_| {
+                        let mut out = Vec::with_capacity(step);
+
+                        for &i in chunk {
+                            out.push(f(i));
+                        }
+
+                        out
+                    })
+                );
+                
+            }
+
+            for i in joins {
+                out_vec.append(i.join().unwrap().as_mut());
+            }
+
+        }).unwrap();
     }
 
     out_vec
@@ -52,11 +66,22 @@ mod test {
             'f', 'e', 'n', 'd', 'e', 'r', ' ', 'F', '1', '0', '2', '0', 'S',
         ];
 
-        let out = split_compitation(vs, &from_char_to_i32);
+        let out = split_compitation(&vs, &from_char_to_i32);
 
         assert_eq!(
             out,
             vec![11, 11, 11, 11, 11, 11, 33, 11, 22, 22, 22, 22, 11]
         );
+    }
+
+    #[test]
+    fn square_test() {
+        let vs = vec![2; 1000];
+
+        let out = split_compitation(&vs, &|i| {
+            i*i*i
+        });
+
+        assert_eq!(out, vec![8;1000]);
     }
 }
