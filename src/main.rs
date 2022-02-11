3b0db7109f67cf7ec25020e38dc2af333974cdc9
Valentin Romanov <22451680+valrom@users.noreply.github.com>
2022-02-11 21:07:52 +0300

    Demo in main with crossbeam and without generics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let vs = vec![10;100];
+    let mut outs = Vec::with_capacity(100);
+
+    let step = 100 / 4;
+
+    crossbeam::scope(|s| {
+        let mut joins = Vec::with_capacity(4);
+
+        for chunk in vs.chunks(step) {
+            joins.push(
+                s.spawn( move |_| {
+                    let mut out = Vec::with_capacity(step);
+
+                    for &i in chunk {
+                        out.push(i*i);
+                    }
+
+                    out
+                })
+            );
+        }
+
+        for i in joins {
+            outs.append(i.join().unwrap().as_mut());
+        }
+
+
+    }).unwrap();
+
+    println!("{:?}\n{:?}", vs, outs);
 }
